436d2cfac741007a2c446822fab7d54c16db7012
danielgnz <danibtechnology@gmail.com>
2019-07-11 16:58:29 +0100

    Updates

---
+++ precache-manifest.f767566ec317e2ad934312f608ad6d59.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b3855af66ecdbe5ad90c7f2417d0fa8",
+    "url": "/raul-finaru/index.html"
+  },
+  {
+    "revision": "864ccf273b6ebf3943ea",
+    "url": "/raul-finaru/static/css/main.63749835.chunk.css"
+  },
+  {
+    "revision": "7796ca45a97c6b0390af",
+    "url": "/raul-finaru/static/js/2.0e689458.chunk.js"
+  },
+  {
+    "revision": "864ccf273b6ebf3943ea",
+    "url": "/raul-finaru/static/js/main.24bd8c18.chunk.js"
+  },
+  {
+    "revision": "ae8f3150c89c10bf538e",
+    "url": "/raul-finaru/static/js/runtime~main.2f34da61.js"
+  },
+  {
+    "revision": "d3a3b80f69080cff919c8e19ac97b156",
+    "url": "/raul-finaru/static/media/connect-with-raul.d3a3b80f.png"
+  },
+  {
+    "revision": "2a7b819280e850b08009378d17ee7d8f",
+    "url": "/raul-finaru/static/media/hero-section.2a7b8192.jpg"
+  },
+  {
+    "revision": "dc04c883fb2ab2ca2e62647d0a85b405",
+    "url": "/raul-finaru/static/media/logo.dc04c883.png"
+  },
+  {
+    "revision": "b4927cdbd19f62661f54fdf26d9f2ac6",
+    "url": "/raul-finaru/static/media/who-is-headshot-closer.b4927cdb.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.73f70adb6aa794e781066941ebd49edb.js"
+  "/raul-finaru/precache-manifest.f767566ec317e2ad934312f608ad6d59.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/raul-finaru/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
